f42dc94bb75268ce7e451444183908eeb8329150
Gugsa <gchalla@uwo.ca>
2020-07-23 00:11:52 -0400

    pushing Live Chat

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const socket = require("socket.io");
+//App setup
+const app = express();
+const server = app.listen(4000, ()=>{
+    console.log("listening to requests");
+});
+
+//Static files
+app.use(express.static("public"));
+
+//Socket setup
+var io = socket(server);
+//number of users in chat counter
+let uCount = 0;
+
+//When user connects online
+io.on("connection", (socket)=>{
+    console.log("made socket connection",socket.id);
+    
+    uCount++;
+    io.sockets.emit("uCount", { uCount: uCount });
+    
+    //When user goes offline
+    socket.on("disconnect", ()=> {
+        uCount--;
+        io.sockets.emit("uCount", { uCount: uCount });
+      });
+
+    socket.on("chat", (data)=>{
+        io.sockets.emit("chat", data);
+    });
+
+    socket.on("typing", (data)=>{
+        socket.broadcast.emit("typing", data);
+    })
+});
+
---
+++ public/chat.js
@@ -0,0 +1,80 @@
+//Make connection
+const socket = io.connect("http://localhost:4000");
+
+const message = document.getElementById("message");
+const name = document.getElementById("name");
+const button = document.getElementById("send");
+const output = document.getElementById("output");
+const typing = document.getElementById("typing");
+const title = document.getElementById("title");
+const count = document.getElementById("count");
+
+//Emit events
+
+//What happens when Send button gets clicked
+button.addEventListener("click", ()=>{
+    let check = name.value.trim();
+    //Check if user's name is blank
+    if(check === ""){
+        alert("You must enter your name before you can send a message!");
+    }
+    //Check if message is empty
+    else if(message.value.trim() === ""){
+        alert("You cannot send an empty message!");
+    }
+    else{
+        title.innerHTML = "<h3>" + name.value + "'s Chat </h3>";
+        socket.emit("chat",{
+            message: message.value,
+            name: name.value
+        });
+        
+        
+        
+        
+        message.value = "";
+        name.value = "";
+    }
+    
+
+
+});
+
+//What happens when user is typing
+message.addEventListener("keypress", ()=>{
+    let space = name.value.trim();
+    //Check if user entered name befpre typing
+    if(space === ""){
+        alert("You must enter your name before you start typing!");
+    }
+    else{
+        socket.emit("typing", {
+            message: message.value,
+            name: name.value
+        });
+    }
+    
+})
+
+
+
+
+//Listen for events
+
+socket.on("chat", (data)=>{
+    output.innerHTML += "<p><strong>" + data.name + ": </strong>" + data.message + "</p>";
+    typing.innerHTML = "";
+});
+
+socket.on("typing", (data)=>{
+        typing.innerHTML = "<p><em>" + data.name + " is typing...</em></p>";
+});
+
+
+socket.on("uCount",(data)=> {
+    console.log(data.uCount);
+    count.innerHTML = "<h3> Number of Users Online: " + data.uCount + "</h3>"
+  });
+
+
+
